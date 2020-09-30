d14ebec4afec37f08e0d000fa8a852c7a19e464d
sayan-datapython <72145939+sayan-datapython@users.noreply.github.com>
2020-09-30 23:11:20 +0530

    Update armstrong_numbers.py
    Now it will work

--- armstrong_numbers.py
+++ armstrong_numbers.py
@@ -1,9 +1,36 @@
 def is_armstrong_number(number):
-    digits = [int(d) for d in str(number)]
-    length = len(digits)
-    result = 0
+    length = len(str(number))
+    temp=num
+    digit=0
+    sum=0
+    while(temp<o):
+        digit=num%10
+        sum=sum+digit**length
+        temp//=10
+    if(sum==num):
+        return(True)
+    else:
+        return(False)
+    
+    def main():
+        num=int(input("Enter a number: "))
+        check=is_armstrong_number(num)
+        if(True):
+            print(f"The (num) is Armstrong number")
+        else:
+            print(f"The number is not Atmstrong number")
+if__name__=='__main__':
+    main()
 
-    for digit in digits:
-        result += digit ** length
 
-    return result == number
+
+        
+    
+  
+    
+
+    
+      
+
+
+    
